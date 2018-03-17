f686f2fc480ff5c5cca18f50fe694f68b7bde819
Mayank Padhi <mayankpadhi@live.com>
2018-03-18 02:49:44 +0530

    not final commit

---
+++ src/pages/about/about.ts
@@ -0,0 +1,30 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Leader } from '../../shared/leader';
+import { LeaderProvider } from '../../providers/leader/leader';
+
+@IonicPage()
+@Component({
+  selector: 'page-about',
+  templateUrl: 'about.html',
+})
+export class AboutPage {
+
+  leaders: Leader[];
+  errMess: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private leaderservice: LeaderProvider,
+    @Inject('BaseURL') private BaseURL ) {
+  }
+
+  ngOnInit() {
+    this.leaderservice.getLeaders()
+      .subscribe(leaders => this.leaders = leaders, errmess => this.errMess = <any>errmess);
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad AboutPage');
+  }
+
+}
